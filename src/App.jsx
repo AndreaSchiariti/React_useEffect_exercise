a18833a4c70cc8109a176ad4c017fc571edf05d3
AndreaSchiariti <schiariti.andrea@gmail.com>
2024-06-18 16:03:37 +0200

    fix: Folder managment

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { LanguageContext } from "../LanguageContext";
+import { LanguageContext } from "./LanguageContext";
 import { Clock } from "./Clock";
-import {useState} from "react"
+import { useState } from "react";
 
 export function App() {
-  const [language, setLanguage] = useState("en")
+  const [language, setLanguage] = useState("en");
 
   return (
     <div>
-      <select name="remember" id="language" onChange={(e) => setLanguage(e.target.value)}>
+      <select
+        name="remember"
+        id="language"
+        onChange={(e) => setLanguage(e.target.value)}
+      >
         <option value="en">English</option>
         <option value="it">Italiano</option>
       </select>
-      <LanguageContext.Provider value={language} >
-      <Clock />
+      <LanguageContext.Provider value={language}>
+        <Clock />
       </LanguageContext.Provider>
     </div>
-  )
+  );
 }
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,26 +1,32 @@
-import { useState } from "react"
-import { useEffect } from "react"
-import { LanguageContext } from "../LanguageContext"
-import {useContext} from "react"
+import { useState } from "react";
+import { useEffect } from "react";
+import { LanguageContext } from "./LanguageContext";
+import { useContext } from "react";
 
 export function Clock() {
-  const [currentTime, setTime] = useState(new Date())
-  const language = useContext(LanguageContext)
+  const [currentTime, setTime] = useState(new Date());
+  const language = useContext(LanguageContext);
 
   useEffect(() => {
     const timeInterval = setInterval(() => {
-      setTime(new Date())
-    }, 1000)
+      setTime(new Date());
+    }, 1000);
 
     /* Il progetto è in Strict Mode, quindi utilizzo una
     cleanup function per fare in modo che l'intervallo si
     interrompa alla seconda costruzione del componente, in
     modo tale da avere un solo processo attivo */
 
-    return (() => {
-      clearInterval(timeInterval)
-    })
-  })
-  
-  return <h2>{language === "en" ? `The current time is ${currentTime.toLocaleTimeString()}`: `Sono le ore ${currentTime.toLocaleTimeString()}`}</h2>
+    return () => {
+      clearInterval(timeInterval);
+    };
+  });
+
+  return (
+    <h2>
+      {language === "en"
+        ? `The current time is ${currentTime.toLocaleTimeString()}`
+        : `Sono le ore ${currentTime.toLocaleTimeString()}`}
+    </h2>
+  );
 }
